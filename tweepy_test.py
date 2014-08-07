9a38c5f6b65316aa52f4330ef50e28138278fd8c
AlisamfP <eatliightning@gmail.com>
2014-08-06 17:35:30 -0700

    added picture of poop

--- tweepy_test.py
+++ tweepy_test.py
@@ -2,10 +2,10 @@ import tweepy
 import json
 
 # Authentication details. To  obtain these visit dev.twitter.com
-consumer_key = 'md9JnOCukpn2r2p46wRp1IcfH'
-consumer_secret = 'Qw9OE2GddvjnALDU9XwIcOMK6Qo1x32zTlUfRuxjW5KI3DIKcU'
-access_token = '115287081-FPXnA1fnjDGHGGIV3dRmUkhdl3xZFfLtTNQvgcpH'
-access_token_secret = '9hbsWS0PLPz0LASoVFIXiWER1wMeAaETx07c7QQY3PH4s'
+consumer_key = 'wxjQaFdC5iCsFjq4RzeqL1H5e'
+consumer_secret = '3Pw8uFcPmhPjFCcmKhsNHFa6qk56GPW1ClkhCy48Xa9joLABem'
+access_token = '2633116405-evYnmdrgC4P1jeUlHNkoitWLzf3hjsEacVT9p6l'
+access_token_secret = 'VHfXcKbop4hedjqKBwSMIizDR4pkN5uh0cHxPzjt3YQwZ'
 
 # This is the listener, resposible for receiving data
 class StdOutListener(tweepy.StreamListener):
@@ -15,8 +15,9 @@ class StdOutListener(tweepy.StreamListener):
 
         # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
         print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
-        print ''
+        print (decoded['id'])
         return True
+
 
     def on_error(self, status):
         print status
@@ -26,10 +27,7 @@ if __name__ == '__main__':
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    print "Showing all new tweets for #poopin:"
 
-    # There are different kinds of streams: public stream, user stream, multi-user streams
-    # In this example follow #programming tag
-    # For more details refer to https://dev.twitter.com/docs/streaming-apis
+    print "Showing all new tweets for #poopin:"
     stream = tweepy.Stream(auth, l)
     stream.filter(track=['#poopin'])
