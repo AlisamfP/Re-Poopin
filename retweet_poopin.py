ad9659395c91cc4da7f0fd7379a649caedda0184
alisamfp <eatliightning@gmail.com>
2014-10-07 21:24:06 -0700

    using heroku config variables!

--- retweet_poopin.py
+++ retweet_poopin.py
@@ -4,10 +4,10 @@ import os
 from flask import Flask
 
 # Authentication details. To  obtain these visit dev.twitter.com
-consumer_key = 'wxjQaFdC5iCsFjq4RzeqL1H5e'
-consumer_secret = '3Pw8uFcPmhPjFCcmKhsNHFa6qk56GPW1ClkhCy48Xa9joLABem'
-access_token = '2633116405-evYnmdrgC4P1jeUlHNkoitWLzf3hjsEacVT9p6l'
-access_token_secret = 'VHfXcKbop4hedjqKBwSMIizDR4pkN5uh0cHxPzjt3YQwZ'
+consumer_key = os.environ['CONS_KEY']
+consumer_secret = os.environ['CONS_SEC']
+access_token = os.environ['ACC_TOKEN']
+access_token_secret = os.environ['TOKEN_SEC']
 
 app = Flask(__name__)
 @app.route('/')
